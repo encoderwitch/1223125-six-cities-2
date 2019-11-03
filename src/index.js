91e04883421ef99f0f1af387514ed1047f4bc82c
Tokar Irina <irina.tokar@hexagon.com>
2019-11-04 01:27:27 +0200

    add new files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainPage from './components/MainPage';
+import App from './components/App';
 
-ReactDOM.render(<MainPage/>, document.getElementById(`root`));
+const init = () => {
+  const list = [
+    `Beautiful & luxurious apartment at great location`,
+    `Wood and stone place`,
+    `Canal View Prinsengracht`,
+    `Nice, cozy, warm big bed apartment`
+  ];
+
+  ReactDOM.render(<App list={list}/>, document.getElementById(`root`));
+ 
+};
+
+init();
